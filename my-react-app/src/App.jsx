1a58f7d091101c84670f7b5f4a4bc6ab71a51d33
Vedat <vedat.arpa.72@gmail.com>
2025-12-21 13:48:23 +0300

    React Practices

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,23 +1,29 @@
-import UserCard from "./components/userCard";
+import UserCard from "./components/UserCard"
+import { userEffect, useState } from "react"
+
 
 function App() {
+  
+  const [users, setUsers] = useState([])
+
+  userEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((data) => setUsers(data))
+  },[])
+
   return (
     <>
-      <UserCard
-        name = "Vedat"
-        job = "Computer Engineering Student"
-        age = {21}
-        isOnline= {true}
-        avatar= "https://i.pravatar.cc/150?img=3"
-      />
+      <h1>User List (API)</h1>
 
-      <UserCard
-      name ="Ahmet"
-      job = "Frontend Developer"
-      age = {25}
-      isOnline={false}
-      avatar="https://i.pravatar.cc/150?img=5"
-      />
+      {userEffect.map((user) => (
+        <UserCard
+          key={user.id}
+          name={user.name}
+          email={user.email}
+          city={user.address.city}
+        />
+      ))}
     </>
   )
 }
--- my-react-app/src/components/UserCard.jsx
+++ my-react-app/src/components/UserCard.jsx
@@ -1,15 +1,9 @@
-function UserCard({ name, job, age, isOnline }){
+function UserCard({ name, email, city }){
   return (
     <div style={style.card}>
-      <img src="{avatar}" alt="{name}" style={style.image} />
-
-      <h2>{name}</h2>
-      <p>{job}</p>
-      <p>Yaş: {age}</p>
-
-      <span style={{ color: isOnline ? 'green' : 'red'}}>
-      {isOnline ? 'Online' : 'Offline'}
-      </span>
+      <h3>{name}</h3>
+      <p>{email}</p>
+      <p>{city}</p>
     </div>
   )
 }
@@ -18,16 +12,10 @@ export default UserCard
 
 
 const style = {
-  card:{
+  card: {
   border: "1px solid #ccc",
-  borderRadius: "10px",
-  padding: "16px",
-  width: "220px",
-  marginBottom: "16px",
-  textAlign: "center"
-  },
-  image: {
-    width: "100px",
-    borderRadius: "50%"
+  borderRadius: "8px",
+  padding: "12px",
+  marginBottom: "12px",
   }
 }
