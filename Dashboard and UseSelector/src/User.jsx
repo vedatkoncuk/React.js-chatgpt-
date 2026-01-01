b30ddd8fe47928bf18cd27f59e7e6822d0754793
Vedat <vedat.arpa.72@gmail.com>
2026-01-01 23:44:36 +0300

    Dispatch and Redux

---
+++ Dashboard and UseSelector/src/User.jsx
@@ -0,0 +1,16 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { updateUser } from "./userSlice";
+
+function User() {
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = () => {
+
+  };
+}
